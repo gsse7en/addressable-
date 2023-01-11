abe7e07df272a0d62a27cf594724a823adafa517
gsse7en <latyshev.fedor@gmail.com>
2023-01-11 20:06:48 +0200

    play video on button click

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.Video;
 
 [RequireComponent(typeof(AudioSource))]
 class Main : MonoBehaviour
@@ -17,6 +18,8 @@ class Main : MonoBehaviour
     private string m_JsonAddress;
     [SerializeField]
     private string m_AudioAddress;
+    [SerializeField]
+    private VideoPlayer m_VideoPlayer;
 
     private AsyncOperationHandle<Material> materialHandle;
     private AsyncOperationHandle<GameObject> prefabHandle;
@@ -39,9 +42,54 @@ class Main : MonoBehaviour
         m_AudioSource = GetComponent<AudioSource>();
     }
 
+    //void Start()
+    //{
+    //    // Will attach a VideoPlayer to the main camera.
+    //    GameObject camera = GameObject.Find("Main Camera");
+
+    //    // VideoPlayer automatically targets the camera backplane when it is added
+    //    // to a camera object, no need to change videoPlayer.targetCamera.
+    //    var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+    //    // Play on awake defaults to true. Set it to false to avoid the url set
+    //    // below to auto-start playback since we're in Start().
+    //    videoPlayer.playOnAwake = false;
+
+    //    // By default, VideoPlayers added to a camera will use the far plane.
+    //    // Let's target the near plane instead.
+    //    videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+
+    //    // This will cause our Scene to be visible through the video being played.
+    //    videoPlayer.targetCameraAlpha = 0.5F;
+
+    //    // Set the video to play. URL supports local absolute or relative paths.
+    //    // Here, using absolute.
+    //    videoPlayer.url = "/Users/graham/movie.mov";
+
+    //    // Skip the first 100 frames.
+    //    videoPlayer.frame = 100;
+
+    //    // Restart from beginning when done.
+    //    videoPlayer.isLooping = true;
+
+    //    // Each time we reach the end, we slow down the playback by a factor of 10.
+    //    videoPlayer.loopPointReached += EndReached;
+
+    //    // Start playback. This means the VideoPlayer may have to prepare (reserve
+    //    // resources, pre-load a few frames, etc.). To better control the delays
+    //    // associated with this preparation one can use videoPlayer.Prepare() along with
+    //    // its prepareCompleted event.
+    //    videoPlayer.Play();
+    //}
+
     public void PlaySound()
     {
         m_AudioSource.PlayOneShot(m_AddressableAudio, 1f);
+    }
+
+    public void PlayVideo()
+    {
+        m_VideoPlayer.Play();
     }
 
     private void PrefabHandle_Completed(AsyncOperationHandle<GameObject> operation)
