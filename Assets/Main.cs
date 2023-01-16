febc6464cc263ab2cefa4e0218b4eef348ebc92b
gsse7en <latyshev.fedor@gmail.com>
2023-01-16 13:58:37 +0200

    add formating and set namespaces

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -4,73 +4,79 @@ using UnityEngine.UI;
 using UnityEngine.Video;
 using System.Threading.Tasks;
 
-[System.Serializable]
-public class JsonSerializedObject
+namespace Addressales.Main
 {
-    public string json_text;
+    [System.Serializable]
+    public class JsonSerializedObject
+    {
+        public string json_text;
+    }
+
+    class Main : MonoBehaviour
+    {
+        [SerializeField] private Text m_Text;
+        [SerializeField] private Image m_Image;
+        [SerializeField] private VideoPlayer m_VideoPlayer;
+        [SerializeField] private string m_PrefabAddress;
+        [SerializeField] private string m_Prefab2Address;
+        [SerializeField] private string m_MaterialAddress;
+        [SerializeField] private string m_JsonAddress;
+        [SerializeField] private string m_AudioAddress;
+        [SerializeField] private string m_VideoAddress;
+        [SerializeField] private string m_SpriteAddress;
+
+        private GameObject m_Prefab;
+        private AudioSource m_AudioSource;
+        private AudioClip m_AddressableAudio;
+        private Sprite m_AddressableSprite;
+
+        #region Lifecycle
+        void Start()
+        {
+            m_AudioSource = GetComponent<AudioSource>();
+
+            LoadAssets();
+        }
+        #endregion
+
+        #region Async
+        async void LoadAssets()
+        {
+            await LoadAssetAsync();
+        }
+
+        async Task LoadAssetAsync()
+        {
+            m_AddressableAudio = await Addressables.LoadAssetAsync<AudioClip>(m_AudioAddress).Task;
+            m_VideoPlayer.clip = await Addressables.LoadAssetAsync<VideoClip>(m_VideoAddress).Task;
+            m_AddressableSprite = await Addressables.LoadAssetAsync<Sprite>(m_SpriteAddress).Task;
+
+            TextAsset json_string = await Addressables.LoadAssetAsync<TextAsset>(m_JsonAddress).Task;
+            GameObject prefab = await Addressables.LoadAssetAsync<GameObject>(m_PrefabAddress).Task;
+            Material material = await Addressables.LoadAssetAsync<Material>(m_MaterialAddress).Task;
+
+            m_Prefab = Instantiate(prefab, transform);
+            m_Prefab.GetComponent<MeshRenderer>().material = material;
+            m_Text.text += JsonUtility.FromJson<JsonSerializedObject>(json_string.ToString()).json_text;
+        }
+        #endregion
+
+        #region Delegates
+        public void PlaySound()
+        {
+            m_AudioSource.PlayOneShot(m_AddressableAudio, 1f);
+        }
+
+        public void PlayVideo()
+        {
+            m_VideoPlayer.Play();
+        }
+
+        public void ShowPicture()
+        {
+            m_Image.sprite = m_AddressableSprite;
+        }
+        #endregion
+    }
 }
 
-[RequireComponent(typeof(AudioSource))]
-class Main : MonoBehaviour
-{
-    [SerializeField] private Text m_Text;
-    [SerializeField] private Image m_Image;
-    [SerializeField] private VideoPlayer m_VideoPlayer;
-    [SerializeField] private string m_PrefabAddress;
-    [SerializeField] private string m_Prefab2Address;
-    [SerializeField] private string m_MaterialAddress;
-    [SerializeField] private string m_JsonAddress;
-    [SerializeField] private string m_AudioAddress;
-    [SerializeField] private string m_VideoAddress;
-    [SerializeField] private string m_SpriteAddress;
-
-    private GameObject m_Prefab;
-    private AudioSource m_AudioSource;
-    private AudioClip m_AddressableAudio;
-    private Sprite m_AddressableSprite;
-
-    #region Lifecycle
-    void Start()
-    {
-        m_AudioSource = GetComponent<AudioSource>();
-        Caller();
-    }
-    #endregion
-
-    #region Async
-    async void Caller()
-    {
-        await LoadAssetAsync();
-    }
-
-    async Task LoadAssetAsync()
-    {
-        TextAsset json_string = await Addressables.LoadAssetAsync<TextAsset>(m_JsonAddress).Task;
-        GameObject prefab = await Addressables.LoadAssetAsync<GameObject>(m_PrefabAddress).Task;
-        Material material = await Addressables.LoadAssetAsync<Material>(m_MaterialAddress).Task;
-        m_Prefab = Instantiate(prefab, transform);
-        m_Prefab.GetComponent<MeshRenderer>().material = material;
-        m_AddressableAudio = await Addressables.LoadAssetAsync<AudioClip>(m_AudioAddress).Task;
-        m_VideoPlayer.clip = await Addressables.LoadAssetAsync<VideoClip>(m_VideoAddress).Task;
-        m_AddressableSprite = await Addressables.LoadAssetAsync<Sprite>(m_SpriteAddress).Task;
-        m_Text.text += JsonUtility.FromJson<JsonSerializedObject>(json_string.ToString()).json_text;
-    }
-    #endregion
-
-    #region Delegates
-    public void PlaySound()
-    {
-        m_AudioSource.PlayOneShot(m_AddressableAudio, 1f);
-    }
-
-    public void PlayVideo()
-    {
-        m_VideoPlayer.Play();
-    }
-
-    public void ShowPicture()
-    {
-        m_Image.sprite = m_AddressableSprite;
-    }
-    #endregion
-}
--- Assets/StartScene.cs
+++ Assets/StartScene.cs
@@ -4,38 +4,46 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.UI;
 
-public class StartScene : MonoBehaviour
+namespace Addressales.StartScene
 {
-    public string m_AddressableSceneAddress;
-    public Button m_StartSceneButton;
-    public GameObject m_PressXText;
-
-    private SceneInstance m_AddressableScene;
-
-    public async void Caller()
+    public class StartScene : MonoBehaviour
     {
-        await LoadAssetAsync();
-    }
+        public string m_AddressableSceneAddress;
+        public Button m_StartSceneButton;
+        public GameObject m_PressXText;
+        private SceneInstance m_AddressableScene;
 
-    async void UnloadScene()
-    {
-        await Addressables.UnloadSceneAsync(m_AddressableScene, true).Task;
-    }
-
-    async Task LoadAssetAsync()
-    {
-        m_AddressableScene = await Addressables.LoadSceneAsync(m_AddressableSceneAddress, UnityEngine.SceneManagement.LoadSceneMode.Additive).Task;
-        m_StartSceneButton.gameObject.SetActive(false);
-        m_PressXText.gameObject.SetActive(true);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.X))
+        #region Lifecycle
+        private void Update()
         {
-            m_StartSceneButton.gameObject.SetActive(true);
-            m_PressXText.gameObject.SetActive(false);
-            UnloadScene();
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                m_StartSceneButton.gameObject.SetActive(true);
+                m_PressXText.gameObject.SetActive(false);
+
+                UnloadScene();
+            }
         }
+        #endregion
+
+        #region Async
+        public async void LoadSceneAsset()
+        {
+            await LoadAssetAsync();
+        }
+
+        async void UnloadScene()
+        {
+            await Addressables.UnloadSceneAsync(m_AddressableScene, true).Task;
+        }
+
+        async Task LoadAssetAsync()
+        {
+            m_AddressableScene = await Addressables.LoadSceneAsync(m_AddressableSceneAddress, UnityEngine.SceneManagement.LoadSceneMode.Additive).Task;
+            m_StartSceneButton.gameObject.SetActive(false);
+            m_PressXText.gameObject.SetActive(true);
+        }
+        #endregion
     }
 }
+
